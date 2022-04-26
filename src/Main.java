cd03a1d282ba41290ec6b6a83ef753817f334ab2
kelavo <diak0n@mail.ru>
2022-04-27 00:00:16 +0300

    One commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 3750;
+        int bonus = 20;
+        int bonusMiles = ticketPrice / bonus;
+
+        System.out.println("Начисленно бонусных миль:" + bonusMiles);
+    }
+}
+
